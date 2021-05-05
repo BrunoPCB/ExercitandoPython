7b407a5c52cd174930bce94a3bfca8f5cad75ef0
Bruno Pontes <brunopcle@gmail.com>
2021-05-04 23:17:09 -0300

    Adicionado primeiros arquivos de exercício, Exercicio 1 e Exercicio 2

---
+++ 1 - Sequencial/Exercicio 1.py
@@ -0,0 +1,16 @@
+"""
+1 - Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+"""
+print("Alô Mundo")
+
+print("Exercício Completo \n")
+
+# Fazendo a mais
+print("Português")
+print("\tOlá Mundo")
+
+print("Inglês")
+print("\tHello World")
+
+print("Japonês")
+print("\tKon'nichiwa sekai")

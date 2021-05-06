153a9d2384e60344acc2b46614b85d1fd6f72df5
Bruno Pontes <brunopcle@gmail.com>
2021-05-05 21:38:52 -0300

    Exercicios resolvidos 3 e 4, e adição de documento com todas os exercicios sequenciais

---
+++ 1 - Sequencial/Exercicio 3.py
@@ -0,0 +1,10 @@
+"""
+3 - Faça um Programa que peça dois números e imprima a soma.
+"""
+# Obtendo o primeiro valor para soma
+numero_1 = int(input("Informe o primeiro número: "))
+# Obtendo o primeiro valor para soma
+numero_2 = int(input("Informe o segundo número: "))
+
+# Imprimindo a soma do valores dentro de uma frase formatada.
+print(f"A soma de {numero_1} + {numero_2} é igual a {numero_1 + numero_2}")
---
+++ 1 - Sequencial/Exercicio 4.py
@@ -0,0 +1,16 @@
+"""
+4 - Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+
+# Pedir 4 notas ao usuário
+num_1 = int(input("Primeira nota: "))
+num_2 = int(input("Segunda nota: "))
+num_3 = int(input("Terceira nota: "))
+num_4 = int(input("Quarta nota: "))
+
+# Realizando o cálculo de média.
+# O cálculo de média é (soma todas notas) / quantidade de notas.
+media = (num_1 + num_2 + num_3 + num_4) / 4
+
+# informando as notas e a média.
+print(f"A média das seguintes notas : {num_1}, {num_2}, {num_3}, {num_4}é {media}")

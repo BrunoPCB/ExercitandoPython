88648566d7f7dd426099b6a93151ac99baf3e8bb
Bruno Pontes <brunopcle@gmail.com>
2021-05-05 21:10:41 -0300

    Resolvido o exercício 2

--- 1 - Sequencial/Exercicio 2.py
+++ 1 - Sequencial/Exercicio 2.py
@@ -1,3 +1,8 @@
 """
 2 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 """
+# Nesta linha a variável "numero"
+numero = input("Informe um número? ")
+
+# Mostrar o valor informado anteriormente
+print("O número informado foi ", numero)

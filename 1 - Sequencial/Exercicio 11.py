f7e720730d8ae6db45ee1feafa8a491a15fb255e
Bruno Pontes <brunopcle@gmail.com>
2021-05-10 19:24:33 -0300

    Exercicios concluídos: 11

--- 1 - Sequencial/Exercicio 11.py
+++ 1 - Sequencial/Exercicio 11.py
@@ -6,4 +6,29 @@ e um número real. Calcule e mostre:
  - a soma do triplo do primeiro
    com o terceiro.
  - o terceiro elevado ao cubo.
+
+Neste caso no qual os calculos já estão definidos e o código
+segue de forma sequencial podemos pedir para o programa
+realiar os cálculos de uma só vez, porém para melhor
+uso do processamento.
 """
+# Pedindo os valores
+int_1 = int(input("Primeiro valor inteiro: "))
+int_2 = int(input("Segundo valor inteiro: "))
+float_1 = float(input("Primeiro valor real: "))
+
+# Calculos
+
+# Primeiro: (int_1 * 2) *  (int_2 / 2)
+calculo_1 = (int_1 * 2) *  (int_2 / 2)
+
+# Segundo: (int_1 * 3) + float_1
+calculo_2 = (int_1 * 3) + float_1
+
+# Terceiro: float_1³
+calculo_3 = pow(float_1, 3)
+
+#Mostrando resultados
+print(f"O produto do dobro do primeiro com metade do segundo: {calculo_1:.2f}")
+print(f"A soma do triplo do primeiro com o terceiro: {calculo_2:.2f}")
+print(f"O terceiro elevado ao cubo: {calculo_3:.2f}")
